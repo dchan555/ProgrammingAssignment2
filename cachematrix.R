74228939038f5a01c59931c9b49eabebaeaa43c7
dchan555 <dchan1@illumina.com>
2015-05-22 13:52:52 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -14,56 +14,6 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
-
-## These two functions just mimic the example function "makeVector",
-## you can use "makeCacheMatrix" to build a "matrix object" which can carry 
-## its inversion. At first, its inversion is NULL. When you use "cacheSolve" to
-## the matrix object for the first time, its inversion will be actually computed 
-## and stored in the matrix object. When you use "cacheSolve" for the second time,
-## its inversion will not be computed and will be retrived from the object since
-## the inversion has been cached.
-
-## This function just like a "construction function" in OOP.It takes a matrix object 
-## as argument, and return a "matrix object" which can cache its inversion.
-## The returned "matrix object" is actually a list, and has several functions as
-## its elements.
 
 
 
-makeVector <- function(x = matrix()) {
-  mat_inv <- NULL ##begins by setting the mean to NULL as a placeholder for a future value
-  set <- function(y) { ##defines a function to set the vector, x, to a new vector, y, and resets the mean, m, to NULL
-    x <<- y
-    m <<- NULL 
-  }
-  get <- function() x ##returns the input matrix
-  set_inverse <- function(inverse) mat_inv <<- inverse ##sets the mean, m, to mean
-  get_inverse <- function() mat_inv ##returns the mean, m
-  list(set = set, get = get, ##returns the vector containing all of the functions just defined
-       set_inverse = set_inverse,
-       get_inverse = get_inverse) 
-}
-
-
-## This function takes a "matrix object" build by "makeCacheMatrix".
-## If the "matrix object" has cached inversion, it will simply return it.
-## If the "matrix object" hasn't cached inversion, it will actually compute 
-## it and store the inversion in the "matrix object".
-
-
-
-cacheSolve <- function(x, ...) {
-  mat_inv <- x$get_inverse()
-  if(!is.null(mat_inv)) {
-    message("getting cached data")
-    return(mat_inv)
-  }
-  data <- x$get()
-  mat_inv <- solve(data, ...)
-  x$set_inverse(mat_inv)
-  mat_inv
-}
